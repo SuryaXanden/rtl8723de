1343eb643f5db2666b1246acf561ed3792472250
Surya U <suryaxanden@gmail.com>
2021-09-18 22:53:32 +0530

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,8 +1,6 @@
-# https://github.com/lwfinger/rtlwifi_new/tree/extended
-
 sudo apt-get install dkms git build-essential
-git clone -b extended https://github.com/lwfinger/rtlwifi_new.git
-sudo dkms add ./rtlwifi_new
-sudo dkms install rtlwifi-new/0.6
-sudo cp -r rtlwifi_new/firmware/rtlwifi/ /lib/firmware/rtlwifi/
-sudo echo "options rtl8723de ant_sel=2" > /etc/modprobe.d/rtl8723de.conf
+git clone https://github.com/smlinux/rtl8723de.git -b current
+sudo dkms add ./rtl8723de
+sudo dkms install rtl8723de/5.1.1.8_21285.20171026_COEX20170111-1414
+sudo depmod -a
+echo Reboot once if needed
